06a771fb8b4717991c1b66b30ade7461178b6b45
tasnias <tasnias@live.unc.edu>
2018-02-10 20:46:42 -0500

    m

--- theworkings.js
+++ theworkings.js
@@ -1,11 +1,11 @@
-<script type="text/javascript">
+
     function processImage() {
         // **********************************************
         // *** Update or verify the following values. ***
         // **********************************************
 
         // Replace the subscriptionKey string value with your valid subscription key.
-        var subscriptionKey = "b7b6d6bad73e4779bdd9d0614bc91b20";
+        var subscriptionKey = "c9ff0ef0cbf2491b8b4c89572347e4dc";
 
         // Replace or verify the region.
         //
@@ -15,11 +15,14 @@
         //
         // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
         // a free trial subscription key, you should not need to change this region.
-        var uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
+        //
+        // Also, if you want to use the celebrities model, change "landmarks" to "celebrities" here and in
+        // uriBuilder.setParameter to use the Celebrities model.
+        var uriBase = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/analyze";
 
         // Request parameters.
         var params = {
-            "visualFeatures": "Categories,Description,Color",
+            "visualFeatures": "Description,Color",
             "details": "",
             "language": "en",
         };
@@ -33,8 +36,8 @@
             url: uriBase + "?" + $.param(params),
 
             // Request headers.
-            beforeSend: function(xhrObj){
-                xhrObj.setRequestHeader("Content-Type","application/json");
+            beforeSend: function(xhrObj) {
+                xhrObj.setRequestHeader("Content-Type", "application/json");
                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
             },
 
@@ -47,6 +50,7 @@
         .done(function(data) {
             // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            //$("#responseTextArea").val("hi");
         })
 
         .fail(function(jqXHR, textStatus, errorThrown) {
@@ -56,25 +60,3 @@
             alert(errorString);
         });
     };
-</script>
-
-<h1>Analyze image:</h1>
-Enter the URL to an image of a natural or artificial landmark, then click the <strong>Analyze image</strong> button.
-<br><br>
-Image to analyze: <input type="text" name="inputImage" id="inputImage" value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
-<button onclick="processImage()">Analyze image</button>
-<br><br>
-<div id="wrapper" style="width:1020px; display:table;">
-    <div id="jsonOutput" style="width:600px; display:table-cell;">
-        Response:
-        <br><br>
-        <textarea id="responseTextArea" class="UIInput" style="width:580px; height:400px;"></textarea>
-    </div>
-    <div id="imageDiv" style="width:420px; display:table-cell;">
-        Source image:
-        <br><br>
-        <img id="sourceImage" width="400" />
-    </div>
-</div>
-</body>
-</html>
